2db6aa720825bc652f5601b08cee109507c10ec1
Ramanath Nayak <rnayak@zynga.com>
2024-02-03 20:30:29 +0530

    treesitter filetype update

--- init.lua
+++ init.lua
@@ -452,7 +452,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'bash', 'csv', 'dockerfile', 'json', 'lua', 'make', 'markdown', 'python', 'sql', 'terraform', 'vim', 'vimdoc', 'yaml' },
+    ensure_installed = { 'bash', 'csv', 'dockerfile', 'json', 'lua', 'make', 'markdown', 'python', 'sql', 'terraform', 'hcl', 'vim', 'vimdoc', 'yaml' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
